f24b4eaa8b54354654e44fbf8bc22fc64318cfae
Vladyslav Honchar <dr.tienam123@gmail.com>
2025-12-14 06:46:55 +0200

    feat(conference): add view route

--- src/Controller/Customer/ConferenceController.php
+++ src/Controller/Customer/ConferenceController.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Customer;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,26 +13,24 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route(name: 'customer.')]
 final class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'homepage.index', methods: ['GET'])]
-    public function index(Request $request): Response
+    #[Route('/conference', name: 'conference.index', methods: ['GET'])]
+    public function index(Request $request, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
         if ($name = $request->query->get('hello')) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
 
-        return new Response(<<<EOF
-<html lang="en">
-                        <body style="height: 100%">
-                        <div style="height: 100%;display:flex;flex-direction:column;justify-content:center;align-items:center">
-                        $greet
-                        <img src="/images/under-construction.gif" alt="Site under construction">
-</div>
+        return $this->render('customer/conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
 
-</body>
-                    </html>
-EOF
-        );
-
+    #[Route(path: '/conference/{id}', name: 'conference.show', methods: ['GET'])]
+    public function show(string $id, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return $this->render('customer/conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference->getId(), 'createdAt' => 'DESC']),
+        ]);
     }
 }
